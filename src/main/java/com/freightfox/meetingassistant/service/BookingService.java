25d33ea01a55961757cd3ea8ab6eef5770437dd2
divyagautam001 <divyashreegautam@gmail.com>
2024-03-09 14:31:33 +0530

    update Booking service

--- src/main/java/com/freightfox/meetingassistant/service/BookingService.java
+++ src/main/java/com/freightfox/meetingassistant/service/BookingService.java
@@ -1,11 +1,9 @@
 package com.freightfox.meetingassistant.service;
 
-import com.freightfox.meetingassistant.Exception.UserNotFoundException;
 import com.freightfox.meetingassistant.entity.Meeting;
 import com.freightfox.meetingassistant.entity.MeetingRequest;
 import com.freightfox.meetingassistant.repository.MeetingRepository;
 import com.freightfox.meetingassistant.repository.UserRepository;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +19,7 @@ public class BookingService {
     @Autowired
     private MeetingService meetingService;
 
-    public Meeting bookMeeting(MeetingRequest meetingRequest){
+    public Meeting bookMeeting(MeetingRequest meetingRequest) {
         Meeting meeting = meetingService.mapMeetingRequestToEntity(meetingRequest);
         return meetingRepository.save(meeting);
     }
